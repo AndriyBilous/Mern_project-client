ab6a9cc10e17d6368bd7adbf596ffa0aab12b5cc
AndriyBilous <andriy.bilous.92@gmail.com>
2023-05-30 13:28:54 +0300

    Client first commit

---
+++ src/utils/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost/3002/api",
+});
+
+instance.interceptors.request.use((config) => {
+  //this action is for checking our token every time we making request
+  config.headers.Authorization = window.localStorage.getItem("token");
+
+  return config;
+});
+
+export default instance;
