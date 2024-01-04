d04e893d87e60411855acb383f57095a9a0e19c1
AndriyBilous <andriy.bilous.92@gmail.com>
2024-01-04 13:43:44 +0200

    Changed margin for MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -21,7 +21,7 @@ export const MainPage = () => {
   }
 
   return (
-    <div className="max-w-[900px] ms-auto py-10">
+    <div className="max-w-[900px] mx-auto py-10">
       <div className="flex justify-between gap-8">
         <div className="flex flex-col gap-10 basis-4/5">
           {posts.length > 0 &&
