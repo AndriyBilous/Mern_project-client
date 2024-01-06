90358b4704d05c5b232ff731cb2a136d6c699fd2
AndriyBilous <andriy.bilous.92@gmail.com>
2024-01-06 13:42:40 +0200

    Bug fixes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -21,7 +21,7 @@ export const MainPage = () => {
   }
 
   return (
-    <div className="max-w-[900px] mx-auto py-10">
+    <div className="max-w-[900px] py-10" style={{ margin: "0 auto" }}>
       <div className="flex justify-between gap-8">
         <div className="flex flex-col gap-10 basis-4/5">
           {posts.length > 0 &&
