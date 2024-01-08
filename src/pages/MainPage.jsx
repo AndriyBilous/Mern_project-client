9717df0ef614bd5def156ab9f46883e90fa0fd39
AndriyBilous <andriy.bilous.92@gmail.com>
2024-01-08 15:48:12 +0200

    Added useState for posts and loading trigger for content shown

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { PostItem } from "../components/PostItem";
 import { useDispatch, useSelector } from "react-redux";
 import { PopularPosts } from "../components/PopularPosts";
 import { getAllPosts } from "../redux/features/post/postSlice";
 
 export const MainPage = () => {
+  const [postsData, setPostsData] = useState("");
   const dispatch = useDispatch();
-  const { posts, popularPosts } = useSelector((state) => state.post);
+  const { posts, popularPosts, loading } = useSelector((state) => state.post);
 
   useEffect(() => {
     dispatch(getAllPosts());
   }, [dispatch]);
 
-  if (posts.length === 0) {
+  useEffect(() => {
+    if (!loading) {
+      setPostsData(posts);
+    }
+  }, [posts, postsData]);
+
+  // useEffect(() => {
+  //   if (loading) {
+  //     return (
+  //       <div className="text-xl text-center text-white py-10">
+  //         Downloading data...
+  //       </div>
+  //     );
+  //   }
+  // }, [posts, postsData]);
+
+  if (loading) {
+    return (
+      <div className="text-xl text-center text-white py-10">
+        Downloading data...
+      </div>
+    );
+  }
+
+  if (postsData.length === 0 && loading === false) {
     return (
       <div className="text-xl text-center text-white py-10">
         There are no posts yet!
@@ -25,8 +50,10 @@ export const MainPage = () => {
       <div className="max-w-[900px] py-10 mx-auto">
         <div className="flex justify-between gap-8">
           <div className="flex flex-col gap-10 basis-4/5">
-            {posts.length > 0 &&
-              posts?.map((post, index) => <PostItem key={index} post={post} />)}
+            {postsData.length > 0 &&
+              postsData?.map((post, index) => (
+                <PostItem key={index} post={post} />
+              ))}
           </div>
           <div className="basis-1/5">
             <div className="text-xs uppercase text-white">Popular</div>
